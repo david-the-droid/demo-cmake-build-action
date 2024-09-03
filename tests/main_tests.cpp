afb88effe4c44c6840650a11bc30237dca4fe9bc
david-the-droid <mayalsdavid@gmail.com>
2024-09-03 16:35:24 +0100

    Adding basic a single test

--- tests/main_tests.cpp
+++ tests/main_tests.cpp
@@ -1,1 +1,24 @@
+#include "MathLib.h"
+#include "MathSqrtLib.h"
+#include "gtest/gtest.h"
 
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+namespace
+{
+class TestMathLib : public ::testing::Test
+{
+    void SetUp() override
+    {
+    }
+};
+} // namespace
+
+TEST_F(TestMathLib, AddNumbers_adds4And5ToGive9_whenCalled)
+{
+    ASSERT_EQ(AddNumbers(4, 5), 9);
+}
